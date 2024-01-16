e826375624b05e517ae06438f46c0eb6391ec4f4
Juan Pablo ROJAS BUSTOS <jrojas9206@gmail.com>
2024-01-16 22:37:47 +0100

    initial game console ui

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ from ui.hangman_ui import Hangman_ui
 
 from games.tictactoe import TicTacToe
 from games.hangman import Hangman
-
-
+from ui.game_console_ui import GameConsole
 
 if __name__ == "__main__":
     app = qtw.QApplication(sys.argv)
@@ -20,7 +19,9 @@ if __name__ == "__main__":
     #ttt_model = TicTacToe()
     #mw = Tictactoe_ui(ttt_model)
     
-    hm = Hangman()
-    mw = Hangman_ui(hm)
+    #hm = Hangman()
+    #Hm_ui = Hangman_ui(hm)
+
+    mw = GameConsole()
 
     sys.exit(app.exec())
