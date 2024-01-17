00157bb239fa9e08fe80434fb85410232ed9eff3
Juan Pablo ROJAS BUSTOS <jrojas9206@gmail.com>
2024-01-17 14:30:22 +0100

    init dot and boxes model

---
+++ games/dot_and_boxes.py
@@ -0,0 +1,10 @@
+class DotAndBoxes:
+
+    GAME_NAME="Dot and boxes"
+
+    def __init__(self) -> None:
+        self._round 
+        self._active_player
+
+    def get_name(self) -> str:
+        return self.GAME_NAME
